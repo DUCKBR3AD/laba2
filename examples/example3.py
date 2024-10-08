f8ac867b7eb86fa372a74adc5a527fe01e1d6f99
DUCKBR3AD <76643973+DUCKBR3AD@users.noreply.github.com>
2024-10-08 21:39:01 +0500

    Пример 3
    Пример функции с одним аргументом

---
+++ examples/example3.py
@@ -0,0 +1,10 @@
+def example(color):
+    if color == "Red":
+        return "Roses are Red"
+    elif color == "Blue":
+        return "Violets are blue"
+    else: 
+        return "Is not part of the track"
+    
+what_is_it = example('Red')
+print(what_is_it)
